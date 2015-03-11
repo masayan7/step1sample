bb46b1365c56b2b4411344363961baab3c79cc9f
masayan7 <masaya.ishigami.5.8@gmail.com>
2015-03-11 11:15:19 +0900

    todoリスト，ローカルファイル

--- todo.js
+++ todo.js
@@ -1,30 +1,36 @@
 $(loaded);
 
 function loaded() {
-  showText();
+      showText();
   // ボタンをクリックしたときに実行するイベントを設定する
   $("#formButton").click(
     // コールバックとしてメソッドを引数にわたす
     function() {
+
       saveText();
       showText();
     });
+     $("#Button").click(
+    function() {
+      cle();
+   });    
+    
 }
-
+function cle(){
+localStorage.clear();
+}
 // 入力された内容をローカルストレージに保存する
-function saveText(if(checktext(text.val())) {
+function saveText() {
   // 時刻をキーにして入力されたテキストを保存する
   var text = $("#formText");
   var time = new Date();
-}
   var val = escapeText(text.val());
-
-  if(checkText(val)){
-  localStorage.setItem(time, escape(val));
+  if(checkText(val)) { 
+  localStorage.setItem(time, text.val());
   // テキストボックスを空にする
   text.val("");
+  }
 }
-
 // ローカルストレージに保存した値を再描画する
 function showText() {
   // すでにある要素を削除する
@@ -32,38 +38,26 @@ function showText() {
   list.children().remove();
   // ローカルストレージに保存された値すべてを要素に追加する
   var key, value, html = [];
+
   for(var i=0, len=localStorage.length; i<len; i++) {
     key = localStorage.key(i);
     value = localStorage.getItem(key);
-    html.push("<p>" + value + "</p>");
+    html.unshift("<p>" + value + "</p>");
   }
   list.append(html.join(''));
 }
 // 文字をエスケープする
 function escapeText(text) {
   return $("<div>").text(text).html();
-}
+   }
 
 // 入力チェックを行う
 function checkText(text) {
   // 文字数が0または20以上は不可
-  if (0 === text.length || 20 < text.length) {
-    alert("文字数は1〜20字にしてください");
+  if (0=== text.length || 20< text.length) {
+    alert("文字数は1から20字にしてください");
     return false;
   }
-
-  // すでに入力された値があれば不可
-  var length = localStorage.length;
-  for (var i = 0; i < length; i++) {
-    var key = localStorage.key(i);
-    var value = localStorage.getItem(key);
-    // 内容が一致するものがあるか比較
-    if (text === value) {
-      alert("同じ内容は避けてください");
-      return false;
-    }
-  }
-
   // すべてのチェックを通過できれば可
   return true;
 }
